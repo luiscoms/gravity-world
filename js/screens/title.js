f4b8bc064d8a13bc41ab926ce061800321186b10
LuisComS <dev@luiscoms.com.br>
2014-05-16 18:29:10 -0300

    Added Enter key to enter the game

--- js/screens/title.js
+++ js/screens/title.js
@@ -45,6 +45,17 @@ game.TitleScreen = me.ScreenObject.extend({
             }),
             3 // z-index
         );
+
+        // change to play state on press Enter or click/tap
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+            if (action === "enter") {
+                // play something on tap / enter
+                // this will unlock audio on mobile devices
+                me.audio.play("cling");
+                me.state.change(me.state.PLAY);
+            }
+        });
     },
 
     update: function () {
@@ -55,8 +66,8 @@ game.TitleScreen = me.ScreenObject.extend({
     },
 
     onDestroyEvent : function () {
-        // me.input.unbindKey(me.input.KEY.ENTER);
+        me.input.unbindKey(me.input.KEY.ENTER);
         // me.input.unbindPointer(me.input.mouse.LEFT);
-        // me.event.unsubscribe(this.handler);
+        me.event.unsubscribe(this.handler);
     }
 });
