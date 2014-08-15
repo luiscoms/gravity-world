ae114d9d85b91d624a6d868a57217db19a7c94ae
LuisComS <dev@luiscoms.com.br>
2014-08-15 09:29:41 -0300

    Added deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#BIN_DIR=$(readlink -f $(dirname $0));
+BIN_DIR=$(pwd)/$(dirname $0);
+DIRFROM="~/tmp/";
+DIRTO="~/www/labs/gravity-world/dist";
+
+echo $BIN_DIR
+cd $BIN_DIR/../
+#npm install
+grunt
+
+ssh luiscoms@luiscoms.com.br "rm -rf "$DIRFROM
+
+scp -r build luiscoms@luiscoms.com.br:~/tmp/
+
+ssh luiscoms@luiscoms.com.br "rsync -ruv "$DIRFROM" "$DIRTO
