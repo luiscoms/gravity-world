f99e0caab084d87feb8a37a4ced73fea457590ef
LuisComS <dev@luiscoms.com.br>
2014-09-02 18:59:45 -0300

    Added final stages

--- js/resources.js
+++ js/resources.js
@@ -63,8 +63,8 @@ game.resources = [
     {name: "stage-01.09", type: "tmx", src: "data/map/stage-01.09.tmx"},
     {name: "stage-01.10", type: "tmx", src: "data/map/stage-01.10.tmx"},
     {name: "stage-01.11", type: "tmx", src: "data/map/stage-01.11.tmx"},
-    {name: "stage-01.12", type: "tmx", src: "data/map/stage-01.01.tmx"},
-    {name: "stage-01.13", type: "tmx", src: "data/map/stage-01.01.tmx"},
-    {name: "stage-01.14", type: "tmx", src: "data/map/stage-01.01.tmx"},
-    {name: "stage-01.15", type: "tmx", src: "data/map/stage-01.01.tmx"},
+    {name: "stage-01.12", type: "tmx", src: "data/map/stage-01.12.tmx"},
+    {name: "stage-01.13", type: "tmx", src: "data/map/stage-01.13.tmx"},
+    {name: "stage-01.14", type: "tmx", src: "data/map/stage-01.14.tmx"},
+    {name: "stage-01.15", type: "tmx", src: "data/map/stage-01.15.tmx"},
 ];
